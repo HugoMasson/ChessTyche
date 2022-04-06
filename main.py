738b1c4245ff201d6741faf739665d1753421a01
HugoMasson <69651314+HugoMasson@users.noreply.github.com>
2022-04-06 09:50:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+#########################################################################
+#																		#
+#				ChessGame (Tyche) 						  	v1.0.0 		#
+#																		#
+#	Author											   Hugo Masson		#
+#																		#
+#	Last Modified (eu date format)						06/04/2022		#
+#																		#
+#																		#
+#	You are free to use this programm as you want						#
+#	if you wish to repost a modified version please put a link			#
+#	to my Github														#
+#					https://github.com/HugoMasson?tab=repositories		#
+#																		#
+#########################################################################
+
+
+
 import game_gui.gui as gui
-
-
-
-
 
 def main():
 	print("Creating game instance...")
@@ -15,3 +29,4 @@ def main():
 if __name__ == '__main__':
 	print("Launching...")
 	main()
+	print("Bye")
